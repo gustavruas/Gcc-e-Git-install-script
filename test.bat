714e4b934b6d63b8c6a8be0dd7b7c33a12a6cd45
gustavruas <110361106+gustavruas@users.noreply.github.com>
2025-10-17 09:49:22 -0300

    Add files via upload

---
+++ test.bat
@@ -0,0 +1,11 @@
+@echo off
+
+echo Instalando git
+winget install "git.git" --silent --accept-package-agreements --accept-source-agreements
+echo Instalando msys2(terminal com gcc)
+winget install --id=MSYS2.MSYS2 -e --silent --accept-package-agreements --accept-source-agreements
+echo Instalando o gcc no msys2
+C:\msys64\msys2_shell.cmd -defterm -no-start -mingw64 -here -c "pacman -S --noconfirm gcc"
+echo Sucesso
+
+pause
