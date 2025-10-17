23492351fdafd431a0c6851dae4050082c94e0af
gustavruas <110361106+gustavruas@users.noreply.github.com>
2025-10-17 10:28:50 -0300

    Atualizar o test.bat

--- test.bat
+++ test.bat
@@ -6,6 +6,9 @@ echo Instalando msys2(terminal com gcc)
 winget install --id=MSYS2.MSYS2 -e --silent --accept-package-agreements --accept-source-agreements
 echo Instalando o gcc no msys2
 C:\msys64\msys2_shell.cmd -defterm -no-start -mingw64 -here -c "pacman -S --noconfirm gcc"
+acho Instalando o gdb no msys2
+C:\msys64\msys2_shell.cmd -defterm -no-start -mingw64 -here -c "pacman -S --noconfirm gdb"
+
 echo Sucesso
 
 pause
