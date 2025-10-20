13b7c3d21b796dec82a9cb51b3c30b9add6d3574
gustavruas <110361106+gustavruas@users.noreply.github.com>
2025-10-20 09:27:29 -0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -4,10 +4,13 @@ winget install "git.git" --silent --accept-package-agreements --accept-source-ag
 Write-Host "Instalando msys2 (terminal com gcc)"
 winget install --id=MSYS2.MSYS2 -e --silent --accept-package-agreements --accept-source-agreements
 
+Write-Host "Update do repositório do msys2"
+Start-Process -Wait -FilePath C:\msys64\msys2_shell.cmd -ArgumentList "-defterm -no-start -mingw64 -here -c 'pacman -Suy'"
+
 Write-Host "Instalando o gcc no msys2"
-Start-Process -Wait -FilePath "C:\msys64\msys2_shell.cmd" -ArgumentList "-defterm", "-no-start", "-mingw64", "-here", "-c", "pacman -S --noconfirm gcc"
+Start-Process -Wait -FilePath C:\msys64\msys2_shell.cmd -ArgumentList "-defterm -no-start -mingw64 -here -c 'pacman -S --noconfirm gcc'"
 
 Write-Host "Instalando o gdb no msys2"
-Start-Process -Wait -FilePath "C:\msys64\msys2_shell.cmd" -ArgumentList "-defterm", "-no-start", "-mingw64", "-here", "-c", "pacman -S --noconfirm gdb"
+Start-Process -Wait -FilePath C:\msys64\msys2_shell.cmd -ArgumentList "-defterm -no-start -mingw64 -here -c 'pacman -S --noconfirm mingw-w64-ucrt-x86_64-gdb'"
 
 Write-Host "Sucesso"
