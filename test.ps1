d00148396d36b14f17c9658d163da6dd9b4d4b02
gustavruas <110361106+gustavruas@users.noreply.github.com>
2025-10-21 08:55:29 -0300

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -5,7 +5,7 @@ Write-Host "Instalando msys2 (terminal com gcc)"
 winget install --id=MSYS2.MSYS2 -e --silent --accept-package-agreements --accept-source-agreements
 
 Write-Host "Update do repositório do msys2"
-Start-Process -Wait -FilePath C:\msys64\msys2_shell.cmd -ArgumentList "-defterm -no-start -mingw64 -here -c 'pacman -Suy'"
+Start-Process -Wait -FilePath C:\msys64\msys2_shell.cmd -ArgumentList "-defterm -no-start -mingw64 -here -c 'pacman --noconfirm -Suy'"
 
 Write-Host "Instalando o gcc no msys2"
 Start-Process -Wait -FilePath C:\msys64\msys2_shell.cmd -ArgumentList "-defterm -no-start -mingw64 -here -c 'pacman -S --noconfirm gcc'"
